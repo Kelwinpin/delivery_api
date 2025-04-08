c4199f13bb1a19f3db60a4296265c56af3b72524
Kelwinpin <kelwincomw@gmail.com>
2025-04-08 10:58:12 -0300

    add route

--- api/routes/index.js
+++ api/routes/index.js
@@ -6,6 +6,7 @@ const authorization = require('../../base/auth/middleware');
 const router = express.Router();
 
 router.use('/g', require('./generalRoutes'));
+router.use('/login', require('../features/login/login.routes'));
 router.use('/companies', require('../features/companies/companies.routes'));
 router.use('/products', require('../features/products/products.routes'));
 router.use('/users', require('../features/users/users.routes'));
